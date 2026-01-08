3335bfa433dc6727e9917424129b790842d31645
playoffthecuff <jmobile@bk.ru>
2026-01-08 19:47:46 +0300

    refactor: change identity

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -1,21 +1,19 @@
-import { Button } from "@/components/ui/button/button";
-import Link from "next/link";
+import { Link } from "@/components/ui/link/link";
+import { Title } from "@/components/ui/typography/title";
 
 export default function Home() {
+	// TODO Change Identify
 	return (
-		<div className="p-4 flex flex-col gap-4 max-w-200 mx-auto">
-			<h1 className="text-8d font-11d">S3 Line</h1>
+		<div
+			className="max-w-200 min-h-full bg-1 mx-auto p-18d flex flex-col gap-8d grow"
+			style={{ width: "clamp(320px, 100%, 800px)" }}
+		>
+			<Title>Smoothed</Title>
 			<p>Copy-paste react UI components</p>
-			<div className="flex flex-col gap-12d">
-				<Link href="/get-started" className="text-primary underline">
-					Get Started
-				</Link>
-				<Link href="/theme-editor" className="underline text-primary">
-					Theme Editor
-				</Link>
-				<Link href="/components" className="underline text-primary">
-					Components
-				</Link>	
+			<div className="flex flex-col gap-8d mt-10d">
+				<Link href="/get-started">Get Started</Link>
+				<Link href="/theme-editor">Theme Editor</Link>
+				<Link href="/components">Components</Link>
 			</div>
 		</div>
 	);
