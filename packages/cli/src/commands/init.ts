d5d4ae61653dfaf07d6d5016c53a964c93580122
playoffthecuff <jmobile@bk.ru>
2026-01-09 08:17:45 +0300

    docs: add todo in cli init

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -3,6 +3,8 @@ import { installDepsIfMissing } from "./registry/deps.js";
 const DEV_DEPS = ['unocss', '@unocss/postcss', '@unocss/preset-wind4'];
 const DEPS = ['@base-ui/react'];
 
+// TODO add default unocss config installation in init command
+
 export const init = async () => {
 	await installDepsIfMissing(DEPS);
 	await installDepsIfMissing(DEV_DEPS, true);
