2feac4dd52c72ca440f87b7b42115b1f53866939
playoffthecuff <jmobile@bk.ru>
2026-01-20 08:10:58 +0300

    feat: add sizes and width to select component

--- apps/website/src/app/components/select/page.tsx
+++ apps/website/src/app/components/select/page.tsx
@@ -2,13 +2,13 @@ import { HighlightedCode } from "@/components/ui/highlighted-code";
 import { Select } from "@/components/ui/select/select";
 import { Title } from "@/components/ui/typography/title";
 
-const items = [
+const options = [
 	{ label: "Choose", value: null },
-	{ label: "One", value: 1 },
-	{ label: "Two", value: 2 },
-	{ label: "Three", value: 3 },
-	{ label: "Four", value: 4 },
-	{ label: "Five", value: 5 },
+	{ label: "One", value: "One" },
+	{ label: "Two", value: "Two" },
+	{ label: "Three", value: "Three" },
+	{ label: "Four", value: "Four" },
+	{ label: "Five", value: "Five" },
 ];
 
 export default function SelectPage() {
@@ -24,7 +24,7 @@ export default function SelectPage() {
 				<p>Control that provides a menu with options to choose from.</p>
 			</div>
 			<div>
-				<Title size={2}>Installation</Title>
+				<Title size={5}>Installation</Title>
 				<HighlightedCode
 					code="bunx @playoffthecuff/smoothed@latest add select"
 					language="yaml"
@@ -32,48 +32,104 @@ export default function SelectPage() {
 				/>
 			</div>
 			<hr className="mt-16d mb-12d" />
-			<Title size={2}>Variants:</Title>
+			<Title size={5}>Variants:</Title>
 			<div className="flex flex-col gap-16d items-start">
 				<div className="flex flex-col gap-12d">
-					<Title size={3}>Size</Title>
-					<Select items={items} size={"s"} />
-					<Select items={items} size={"m"} />
-					<Select items={items} size={"l"} />
+					<Title size={4}>Size</Title>
+					<div>
+						Extra Small - XS
+						<Select options={options} size={"xs"} />
+					</div>
+					<div>
+						Small - S
+						<Select options={options} size={"s"} />
+					</div>
+					<div>
+						Medium - M
+						<Select options={options} size={"m"} />
+					</div>
+					<div>
+						Large - L
+						<Select options={options} size={"l"} />
+					</div>
+					<div>
+						Extra Large - XL
+						<Select options={options} size={"xl"} />
+					</div>
+				</div>
+				<div className="flex flex-col gap-12d w-full">
+					<Title size={4}>Width</Title>
+					<div>
+						Narrow
+						<Select options={options} width={"narrow"} />
+					</div>
+					<div>
+						Normal
+						<Select options={options} width={"normal"} />
+					</div>
+					<div>
+						Wide
+						<Select options={options} width={"wide"} />
+					</div>
+					<div>
+						Fill
+						<Select options={options} width={"fill"} />
+					</div>
 				</div>
 				<div className="flex flex-col gap-12d">
-					<Title size={3}>Shape</Title>
-					<Select items={items} shape={"square"} />
-					<Select items={items} shape={"rounded"} />
-					<Select items={items} shape={"circular"} />
+					<Title size={4}>Intent</Title>
+					<div>
+						Neutral
+						<Select options={options} intent={"neutral"} />
+					</div>
+					<div>
+						Secondary
+						<Select options={options} intent={"secondary"} />
+					</div>
 				</div>
 				<div className="flex flex-col gap-12d">
-					<Title size={3}>Status</Title>
+					<Title size={4}>Shape</Title>
+					<div>
+						Square
+						<Select options={options} shape={"square"} />
+					</div>
+					<div>
+						Rounded
+						<Select options={options} shape={"rounded"} />
+					</div>
+					<div>
+						Circular
+						<Select options={options} shape={"circular"} />
+					</div>
+				</div>
+				<div className="flex flex-col gap-12d">
+					<Title size={4}>Status</Title>
 					<div>
 						Valid
-						<Select items={items} status={"valid"} />
+						<Select options={options} status={"valid"} />
 					</div>
 					<div>
 						Warning
-						<Select items={items} status={"warning"} />
+						<Select options={options} status={"warning"} />
 					</div>
 					<div>
 						Invalid
-						<Select items={items} status={"invalid"} />
+						<Select options={options} status={"invalid"} />
 					</div>
 				</div>
 				<div className="flex flex-col gap-12d">
-					<Title size={3}>State</Title>
+					<Title size={4}>State</Title>
 					<div>
 						Disabled
-						<Select items={items} disabled />
+						<Select options={options} disabled />
 					</div>
 					<div>
 						Loading
-						<Select items={items} loading />
+						<Select options={options} loading />
 					</div>
 					<div>
 						Disabled & Loading
-						<Select items={items} disabled loading />
+						<Select options={options} disabled loading />
 					</div>
 				</div>
 			</div>
--- apps/website/src/components/ui/select/select.tsx
+++ apps/website/src/components/ui/select/select.tsx
@@ -1,71 +1,99 @@
 import { Select as BaseSelect } from "@base-ui/react/select";
 import { cva, type VariantProps } from "class-variance-authority";
+import type { ReactNode } from "react";
 import { cn } from "@/lib/utils/cn";
 import { CheckIcon } from "../icons/check";
 import { ChevronUpDownIcon } from "../icons/chevron-up-down";
 import { SpinnerIcon } from "../icons/spinner";
 
-const selectTriggerVariants = cva(
-	"w-full h-[2em] disabled:pointer-events-none",
-	{
-		variants: {
-			size: {
-				s: "text-3d font-10d",
-				m: "text-4d font-9d",
-				l: "text-5d font-8d",
-			},
-			shape: {
-				rounded: null,
-				square: null,
-				circular: "rounded-[1em]",
-			},
-			disabled: {
-				true: "opacity-50 saturate-50 contrast-60 pointer-events-none",
-			},
-			loading: {
-				true: "text-transparent cursor-wait pointer-events-none",
-				false: "cursor-pointer",
-			},
-			status: {
-				valid:
-					"border-success text-success focus-visible:shadow-focus-success focus-visible:outline-success-d",
-				warning:
-					"border-warning text-warning focus-visible:shadow-focus-warning focus-visible:outline-warning-d",
-				invalid:
-					"border-error text-error focus-visible:shadow-focus-error focus-visible:outline-error-d",
-			},
+const selectTriggerVariants = cva("w-full disabled:pointer-events-none", {
+	variants: {
+		size: {
+			xs: "text-3d font-10d h-[1.8em]",
+			s: "text-3d font-10d h-[2em]",
+			m: "text-4d font-9d h-[2em]",
+			l: "text-5d font-8d h-[2em]",
+			xl: "text-6d font-8d h-[2em]",
 		},
-		compoundVariants: [
-			{
-				status: undefined,
-				className:
-					"surface-secondary-outline focus-visible:shadow-focus-secondary",
-			},
-			{
-				size: "s",
-				shape: "rounded",
-				className: "rounded-7d",
-			},
-			{
-				size: "m",
-				shape: "rounded",
-				className: "rounded-8d",
-			},
-			{
-				size: "l",
-				shape: "rounded",
-				className: "rounded-9d",
-			},
-		],
-		defaultVariants: {
-			size: "m",
-			shape: "rounded",
-			loading: false,
+		intent: {
+			neutral: null,
+			secondary: null,
+		},
+		width: {
+			narrow: null,
+			normal: null,
+			wide: null,
+			fill: null,
+		},
+		shape: {
+			rounded: null,
+			square: null,
+			circular: "rounded-[1em]",
+		},
+		disabled: {
+			true: "opacity-50 saturate-50 contrast-60 pointer-events-none",
+		},
+		loading: {
+			true: "text-transparent cursor-wait pointer-events-none",
+			false: "cursor-pointer",
+		},
+		status: {
+			valid:
+				"border-success text-success focus-visible:shadow-focus-success focus-visible:outline-success-d",
+			warning:
+				"border-warning text-warning focus-visible:shadow-focus-warning focus-visible:outline-warning-d",
+			invalid:
+				"border-error text-error focus-visible:shadow-focus-error focus-visible:outline-error-d",
 		},
 	},
-);
+	compoundVariants: [
+		{
+			status: undefined,
+			intent: "secondary",
+			className:
+				"surface-secondary-outline focus-visible:shadow-focus-secondary",
+		},
+		{
+			status: undefined,
+			intent: "neutral",
+			className:
+				"surface-foreground-outline focus-visible:shadow-focus-foreground",
+		},
+		{
+			size: "xs",
+			shape: "rounded",
+			className: "rounded-6d",
+		},
+		{
+			size: "s",
+			shape: "rounded",
+			className: "rounded-7d",
+		},
+		{
+			size: "m",
+			shape: "rounded",
+			className: "rounded-8d",
+		},
+		{
+			size: "l",
+			shape: "rounded",
+			className: "rounded-9d",
+		},
+		{
+			size: "xl",
+			shape: "rounded",
+			className: "rounded-10d",
+		},
+	],
+	defaultVariants: {
+		size: "m",
+		shape: "rounded",
+		loading: false,
+		intent: "neutral",
+	},
+});
 const popupVariants = cva(
-	"group min-w-[var(--anchor-width)] origin-[var(--transform-origin)] bg-clip-padding shadow-2d surface-secondary-outline bg-3 transition-[transform,scale,opacity] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[side=none]:min-w-[calc(var(--anchor-width)+1rem)] data-[side=none]:data-[ending-style]:transition-none data-[starting-style]:scale-90 data-[starting-style]:opacity-0 data-[side=none]:data-[starting-style]:scale-100 data-[side=none]:data-[starting-style]:opacity-100 data-[side=none]:data-[starting-style]:transition-none outline-none",
+	"group min-w-[var(--anchor-width)] origin-[var(--transform-origin)] bg-clip-padding shadow-2d surface-secondary-outline transition-[transform,scale,opacity] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[side=none]:min-w-[calc(var(--anchor-width)+1rem)] data-[side=none]:data-[ending-style]:transition-none data-[starting-style]:scale-90 data-[starting-style]:opacity-0 data-[side=none]:data-[starting-style]:scale-100 data-[side=none]:data-[starting-style]:opacity-100 data-[side=none]:data-[starting-style]:transition-none outline-none",
 	{
 		variants: {
 			shape: {
@@ -73,10 +101,16 @@ const popupVariants = cva(
 				rounded: null,
 				circular: "rounded-[1em]",
 			},
+			intent: {
+				neutral: "bg-3",
+				secondary: "bg-2",
+			},
 			size: {
+				xs: "text-3d font-10d",
 				s: "text-3d font-10d",
 				m: "text-4d font-9d",
 				l: "text-5d font-8d",
+				xl: "text-6d font-8d",
 			},
 		},
 		compoundVariants: [
@@ -99,6 +133,7 @@ const popupVariants = cva(
 		defaultVariants: {
 			shape: "rounded",
 			size: "m",
+			intent: "neutral",
 		},
 	},
 );
@@ -110,41 +145,81 @@ const triggerWrapperVariants = cva("flex items-center justify-center", {
 		loading: {
 			true: "cursor-wait",
 		},
+		width: {
+			narrow: "w-[13ch]",
+			normal: "w-[18ch]",
+			wide: "w-[25ch]",
+			fill: "w-full",
+		},
+	},
+	defaultVariants: {
+		width: "normal",
 	},
 });
 
+interface Option {
+	label: React.ReactNode;
+	value: string | number | null;
+}
+
 export type SelectProps = VariantProps<typeof selectTriggerVariants> & {
-	items: { label: string; value: string | number | null }[];
-} & { required?: boolean; id?: string };
+	options: Option[];
+	required?: boolean;
+	placeholder?: BaseSelect.Value.Props["placeholder"];
+	id?: string;
+	children?: ReactNode;
+	defaultValue?: Option;
+};
 
 export function Select({
-	items,
+	options,
 	size,
 	shape,
 	loading,
 	disabled,
+	placeholder,
+	width,
 	status,
 	required,
+	intent,
+	defaultValue,
+	id,
 }: SelectProps) {
+	const isNumberOptions = typeof options?.[0]?.value === "number";
 	return (
 		<BaseSelect.Root
-			items={items}
+			items={options}
 			disabled={disabled ?? undefined}
 			required={required}
+			id={id}
+			defaultValue={defaultValue}
 		>
-			<span className={triggerWrapperVariants({ disabled, loading })}>
+			<span className={triggerWrapperVariants({ disabled, loading, width })}>
 				{loading && (
 					<SpinnerIcon className="animate-spin absolute pointer-events-none" />
 				)}
 				<BaseSelect.Trigger
 					className={cn(
-						"flex min-w-28d px-[1.2ch] items-center justify-between gap-3 border-1.6 bg-3 select-none hover:bg-2",
-						selectTriggerVariants({ size, shape, disabled, loading, status }),
+						"flex ps-[1ch] items-center justify-between gap-[1ch] border-3d select-none",
+						(intent === "neutral" || !intent) && "bg-3 hover:bg-2",
+						intent === "secondary" && "bg-2 hover:bg-1",
+						isNumberOptions && "font-mono",
+						selectTriggerVariants({
+							size,
+							shape,
+							disabled,
+							loading,
+							status,
+							intent,
+						}),
 					)}
 				>
-					<BaseSelect.Value />
-					<BaseSelect.Icon className="flex">
-						<ChevronUpDownIcon size="1em" />
+					<BaseSelect.Value
+						placeholder={placeholder}
+						className={"data-[placeholder]:text-muted-3"}
+					/>
+					<BaseSelect.Icon className="flex w-[2em] justify-center">
+						<ChevronUpDownIcon className="stroke-3d" />
 					</BaseSelect.Icon>
 				</BaseSelect.Trigger>
 			</span>
@@ -153,15 +228,16 @@ export function Select({
 					className="outline-none select-none z-10"
 					sideOffset={8}
 				>
-					<BaseSelect.Popup className={popupVariants({ shape, size })}>
-						<BaseSelect.ScrollUpArrow className="top-0 left-0 z-[1] flex h-[2em] rounded-t-[1em] w-full items-center justify-center bg-[canvas] text-center before:absolute data-[side=none]:before:top-[-100%] before:left-0 before:h-full before:w-full before:content-['']" />
+					<BaseSelect.Popup className={popupVariants({ shape, size, intent })}>
+						<BaseSelect.ScrollUpArrow className="top-0 left-0 z-[1] flex h-[2em] rounded-t-[1em] w-full items-center justify-center bg-[canvas] text-center" />
 						<BaseSelect.List className="relative scroll-py-6 overflow-y-auto max-h-[var(--available-height)]">
-							{items.map(({ label, value }) => (
+							{options.map(({ value }) => (
 								<BaseSelect.Item
-									key={label}
+									key={value}
 									value={value}
 									className={cn(
-										"grid grid-cols-[1.75em_1fr] items-center select-none group-data-[side=none]:pr-12 group-data-[side=none]:leading-4 data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:surface-foreground-solid data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:bg-primary-500d outline-none",
+										"grid grid-cols-[1.75em_1fr] items-center select-none group-data-[side=none]:pr-12 group-data-[side=none]:leading-4 data-[highlighted]:surface-foreground-solid data-[highlighted]:before:z-[-1] outline-none",
+										isNumberOptions && "font-mono",
 										selectTriggerVariants({ shape, size }),
 									)}
 								>
@@ -169,12 +245,12 @@ export function Select({
 										<CheckIcon size="1.125em" />
 									</BaseSelect.ItemIndicator>
 									<BaseSelect.ItemText className="col-start-2">
-										{label}
+										{value}
 									</BaseSelect.ItemText>
 								</BaseSelect.Item>
 							))}
 						</BaseSelect.List>
-						<BaseSelect.ScrollDownArrow className="bottom-0 left-0 z-[1] flex h-[2em] w-full cursor-default items-center justify-center rounded-b-[1em] bg-[canvas] text-center text-xs before:absolute before:left-0 before:h-full before:w-full before:content-[''] bottom-0 data-[side=none]:before:bottom-[-100%]" />
+						<BaseSelect.ScrollDownArrow className="bottom-0 left-0 z-[1] flex h-[2em] w-full items-center justify-center rounded-b-[1em] bg-[canvas] text-center" />
 					</BaseSelect.Popup>
 				</BaseSelect.Positioner>
 			</BaseSelect.Portal>
