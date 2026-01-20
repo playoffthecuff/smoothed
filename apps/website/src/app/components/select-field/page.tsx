f8f0ac4d55a2c0f253ef26be27913b21f26485b3
playoffthecuff <jmobile@bk.ru>
2026-01-20 08:22:58 +0300

    feat: add sizes width and popover message to select field component

--- apps/website/src/app/components/select-field/page.tsx
+++ apps/website/src/app/components/select-field/page.tsx
@@ -2,7 +2,7 @@ import { SelectField } from "@/components/ui/fields/select";
 import { HighlightedCode } from "@/components/ui/highlighted-code";
 import { Title } from "@/components/ui/typography/title";
 
-const items = [
+const options = [
 	{ label: "Choose", value: null },
 	{ label: "One", value: 1 },
 	{ label: "Two", value: 2 },
@@ -27,7 +27,7 @@ export default function SelectFieldPage() {
 				</p>
 			</div>
 			<div>
-				<Title size={2}>Installation</Title>
+				<Title size={5}>Installation</Title>
 				<HighlightedCode
 					code="bunx @playoffthecuff/smoothed@latest add select-field"
 					language="yaml"
@@ -35,141 +35,197 @@ export default function SelectFieldPage() {
 				/>
 			</div>
 			<hr className="mt-16d mb-12d" />
-			<Title size={2}>Variants:</Title>
-			<div className="flex flex-col gap-16d items-start w-60">
+			<Title size={5}>Variants:</Title>
+			<div className="flex flex-col gap-16d items-start">
 				<div>
-					<Title size={3}>Options</Title>
+					<Title size={4}>Options</Title>
 					<div className="flex flex-col gap-16d">
-						<SelectField label="Only Label" id="ol" items={items} />
-						<SelectField label="Hint" items={items} hint="Hint" id="hint" />
-						<SelectField
-							label="Required"
-							required
-							id="required"
-							items={items}
-						/>
+						<SelectField label="Only Label" options={options} />
+						<SelectField label="Hint" options={options} hint="Hint" />
+						<SelectField label="Required" required options={options} />
 						<SelectField
 							label="With message"
 							message="message"
-							items={items}
-							id="wm"
+							options={options}
+						/>
+						<SelectField
+							label="Popover Message"
+							popoverMessage="Popover message"
+							options={options}
 						/>
 					</div>
 				</div>
 				<div>
-					<Title size={3}>Size</Title>
+					<Title size={4}>Size</Title>
 					<div className="flex flex-col gap-16d">
+						<SelectField
+							label="Extra Small"
+							message="XS - Extra Small"
+							size="xs"
+							options={options}
+							hint="Extra Small"
+						/>
 						<SelectField
 							label="Small"
 							message="S - Small"
 							size="s"
-							items={items}
+							options={options}
 							hint="Small"
-							id="small"
 						/>
 						<SelectField
 							label="Medium"
 							message="M - Medium"
 							size="m"
-							items={items}
+							options={options}
 							hint="Medium"
-							id="medium"
 						/>
 						<SelectField
 							label="Large"
-							message="L - large"
+							message="L - Large"
 							size="l"
-							items={items}
+							options={options}
 							hint="Large"
-							id="large"
+						/>
+						<SelectField
+							label="Extra Large"
+							message="XL - Extra Large"
+							size="xl"
+							options={options}
+							hint="Large"
+						/>
+					</div>
+				</div>
+				<div className="w-full">
+					<Title size={4}>Width</Title>
+					<div className="flex flex-col gap-16d">
+						<SelectField
+							label="narrow"
+							message="narrow"
+							width={"narrow"}
+							options={options}
+							hint="narrow"
+						/>
+						<SelectField
+							label="normal"
+							message="normal"
+							width={"normal"}
+							size="m"
+							options={options}
+							hint="normal"
+						/>
+						<SelectField
+							label="Wide"
+							message="wide"
+							width={"wide"}
+							size="l"
+							options={options}
+							hint="Wide"
+						/>
+						<SelectField
+							label="Fill"
+							message="fill"
+							width={"fill"}
+							size="l"
+							options={options}
+							hint="Fill"
 						/>
 					</div>
 				</div>
 				<div>
-					<Title size={3}>Status</Title>
+					<Title size={4}>Intent</Title>
+					<div className="flex flex-col gap-16d">
+						<SelectField
+							label="Neutral"
+							message="neutral"
+							intent={"neutral"}
+							options={options}
+							hint="neutral"
+						/>
+						<SelectField
+							label="Secondary"
+							message="secondary"
+							intent={"secondary"}
+							options={options}
+							hint="secondary"
+						/>
+					</div>
+				</div>
+				<div>
+					<Title size={4}>Status</Title>
 					<div className="flex flex-col gap-16d">
 						<SelectField
 							label="Valid"
 							message="valid"
-							items={items}
+							options={options}
 							hint="Valid"
 							status={"valid"}
-							id="valid"
 						/>
 						<SelectField
 							label="Warning"
 							message="warning"
-							items={items}
+							options={options}
 							hint="Warning"
 							status={"warning"}
-							id="warning"
 						/>
 						<SelectField
 							label="Invalid"
 							message="invalid"
-							items={items}
+							options={options}
 							hint="Invalid"
 							status={"invalid"}
-							id="invalid"
 						/>
 					</div>
 				</div>
 				<div>
-					<Title size={3}>Shape</Title>
+					<Title size={4}>Shape</Title>
 					<div className="flex flex-col gap-16d">
 						<SelectField
 							label="Square"
 							message="square"
-							items={items}
+							options={options}
 							hint="Square"
 							shape={"square"}
-							id="square"
 						/>
 						<SelectField
 							label="Rounded"
 							message="rounded"
-							items={items}
+							options={options}
 							hint="Rounded"
 							shape={"rounded"}
-							id="rounded"
 						/>
 						<SelectField
 							label="Circular"
 							message="circular"
-							items={items}
+							options={options}
 							hint="Circular"
 							shape={"circular"}
-							id="circular"
 						/>
 					</div>
 				</div>
 				<div>
-					<Title size={3}>State</Title>
+					<Title size={4}>State</Title>
 					<div className="flex flex-col gap-16d">
 						<SelectField
 							label="Label"
 							message="Disabled"
-							items={items}
+							options={options}
 							disabled
 							hint="Disabled"
-							id="disabled"
 						/>
 						<SelectField
 							label="Label"
 							message="Loading"
-							items={items}
+							options={options}
 							loading
 							hint="Loading"
-							id="loading"
 						/>
 						<SelectField
 							label="Label"
 							message="Disable & Loading"
-							items={items}
+							options={options}
 							disabled
 							loading
 							hint="Disabled & Loading"
-							id="dnl"
 						/>
 					</div>
 				</div>
--- apps/website/src/components/ui/fields/select.tsx
+++ apps/website/src/components/ui/fields/select.tsx
@@ -1,12 +1,20 @@
+import { cva } from "class-variance-authority";
+import { cn } from "@/lib/utils/cn";
 import { ErrorIcon } from "../icons/error";
 import { SuccessIcon } from "../icons/success";
 import { WarningIcon } from "../icons/warning";
 import { IntentMessage } from "../intent-message/intent-message";
 import { Label } from "../label/label";
+import { Popover, PopoverPortal, PopoverTrigger } from "../popover/popover";
 import { Select, type SelectProps } from "../select/select";
 import { HintButton } from "./hint-button";
 
-type Props = SelectProps & { label: string; message?: string; hint?: string };
+export type SelectFieldProps = SelectProps & {
+	label: string;
+	message?: string;
+	popoverMessage?: string;
+	hint?: string;
+};
 
 const statusToIntent = {
 	valid: "success",
@@ -20,42 +28,79 @@ const intentToIcon = {
 	error: ErrorIcon,
 };
 
+const intentMessageWrapperVariants = cva(null, {
+	variants: {
+		size: {
+			xs: "mt-6d",
+			s: "mt-7d",
+			m: "mt-8d",
+			l: "mt-9d",
+			xl: "mt-10d",
+		},
+	},
+	defaultVariants: {
+		size: "m",
+	},
+});
+
 export function SelectField({
 	hint,
 	label,
 	message,
+	popoverMessage,
 	size,
 	required,
 	shape,
+	width,
 	id,
 	...selectProps
-}: Props) {
+}: SelectFieldProps) {
 	const intent = selectProps.status
 		? statusToIntent[selectProps.status]
 		: undefined;
+	const intentMessage = (
+		<IntentMessage
+			intent={intent}
+			size={size}
+			iconStart={intent ? intentToIcon[intent] : undefined}
+		>
+			{popoverMessage ? popoverMessage : message}
+		</IntentMessage>
+	);
+	const select = (
+		<Select
+			size={size}
+			{...selectProps}
+			required={required}
+			shape={shape}
+			width={width}
+			id={id}
+		/>
+	);
 	return (
-		<div className="flex flex-col w-max">
-			<div className="flex justify-between items-center">
+		<div className={cn("flex flex-col", width !== "fill" && "w-min")}>
+			<div className="flex items-center justify-between me-2d">
 				<Label intent={intent} size={size} required={required} htmlFor={id}>
 					{label}
 				</Label>
-				{hint && <HintButton description={hint} size={size} shape={shape} />}
+				{hint && (
+					<HintButton size={size} shape={shape}>
+						{hint}
+					</HintButton>
+				)}
 			</div>
-			<Select
-				size={size}
-				{...selectProps}
-				required={required}
-				shape={shape}
-				id={id}
-			/>
+			{popoverMessage ? (
+				<Popover open>
+					<PopoverTrigger>{select}</PopoverTrigger>
+					<PopoverPortal>{intentMessage}</PopoverPortal>
+				</Popover>
+			) : (
+				select
+			)}
 			{message && (
-				<IntentMessage
-					intent={intent}
-					size={size}
-					iconStart={intent ? intentToIcon[intent] : undefined}
-				>
-					{message}
-				</IntentMessage>
+				<span className={intentMessageWrapperVariants({ size })}>
+					{intentMessage}
+				</span>
 			)}
 		</div>
 	);
