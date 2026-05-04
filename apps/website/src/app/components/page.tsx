a14831d1a955a3ffdfdd4df420ba1f6b15fca3eb
playoffthecuff <jmobile@bk.ru>
2026-05-04 22:58:09 +0300

    refactor: pages

--- apps/website/src/app/components/page.tsx
+++ apps/website/src/app/components/page.tsx
@@ -1,31 +1,132 @@
-import { Link } from "@/components/ui/link/link";
-import { Title } from "@/components/ui/typography/title";
+import { Link } from "@/components/ui/navigation/link/link";
+import { T } from "@/components/ui/typography";
 
 export default function ComponentsPage() {
 	return (
 		<div
-			className="max-w-200 min-h-full bg-1 mx-auto p-18d flex gap-10d self-start flex-col overflow-x-auto grow"
+			className="max-w-200 min-h-full surface-1 mx-auto p-18d flex gap-10d self-start flex-col overflow-x-auto grow rel-elevation-5 sfc-default sfc-shadow"
 			style={{ width: "clamp(320px, 100%, 800px)" }}
 		>
-			<Title>Components</Title>
-			<Link href={"/components/button"}>Button</Link>
-			<Link href={"/components/card"}>Card</Link>
-			<Link href={"/components/checkbox"}>Checkbox</Link>
-			<Link href={"/components/checkbox-field"}>Checkbox Field</Link>
-			<Link href={"/components/dialog"}>Dialog</Link>
-			<Link href={"/components/icons"}>Icons</Link>
-			<Link href={"/components/intent-message"}>Intent Message</Link>
-			<Link href={"/components/label"}>Label</Link>
-			<Link href={"/components/number-field"}>Number Field</Link>
-			<Link href={"/components/number-input"}>Number Input</Link>
-			<Link href={"/components/popover"}>Popover</Link>
-			<Link href={"/components/select"}>Select</Link>
-			<Link href={"/components/select-field"}>Select Field</Link>
-			<Link href={"/components/slider"}>Slider</Link>
-			<Link href={"/components/slider-field"}>Slider Field</Link>
-			<Link href={"/components/text-field"}>Text Field</Link>
-			<Link href={"/components/text-input"}>Text Input</Link>
-			<Link href={"/components/toggle"}>Toggle</Link>
+			<T.Title>Components</T.Title>
+			<nav>
+				<ul className="flex flex-col gap-12d">
+					<li className="ml-18d">
+						<ul>
+							<T.Title size={"xl"} as="h2">
+								Data Display
+							</T.Title>
+							<li className="ml-18d">
+								<Link href={"/components/data-display/card"}>Card</Link>
+							</li>
+						</ul>
+					</li>
+					<li className="ml-18d">
+						<T.Title size={"xl"} as="h2">
+							Data Entry
+						</T.Title>
+
+						<ul>
+							<li className="ml-18d">
+								<Link href={"/components/data-entry/checkbox-field"}>
+									Checkbox Field
+								</Link>
+							</li>
+							<li className="ml-18d">
+								<Link href={"/components/data-entry/number-field"}>
+									Number Field
+								</Link>
+							</li>
+							<li className="ml-18d">
+								<Link href={"/components/data-entry/select-field"}>
+									Select Field
+								</Link>
+							</li>
+							<li className="ml-18d">
+								<Link href={"/components/data-entry/slider-field"}>
+									Slider Field
+								</Link>
+							</li>
+							<li className="ml-18d">
+								<Link href={"/components/data-entry/text-field"}>
+									Text Field
+								</Link>
+							</li>
+						</ul>
+					</li>
+					<li className="ml-18d">
+						<ul>
+							<T.Title size={"xl"} as="h2">
+								Icons
+							</T.Title>
+							<li className="ml-18d">
+								<Link href={"/components/icons"}>Icons</Link>
+							</li>
+						</ul>
+					</li>
+					<li className="ml-18d">
+						<ul>
+							<T.Title size={"xl"} as="h2">
+								Navigation
+							</T.Title>
+							<ul>
+								<li className="ml-18d">
+									<Link href={"/components/navigation/link"}>Link</Link>
+								</li>
+							</ul>
+						</ul>
+					</li>
+					<li className="ml-18d">
+						<T.Title size={"xl"} as="h2">
+							Triggers
+						</T.Title>
+						<ul>
+							<li className="ml-18d">
+								<Link href={"/components/triggers/button"}>Button</Link>
+							</li>
+							<li className="ml-18d">
+								<Link href={"/components/triggers/toggle"}>Toggle</Link>
+							</li>
+							<li className="ml-18d">
+								<Link href={"/components/triggers/toggle-group"}>
+									Toggle Group
+								</Link>
+							</li>
+						</ul>
+					</li>
+
+					<li className="ml-18d">
+						<ul>
+							<T.Title size={"xl"} as="h2">
+								Feedback
+							</T.Title>
+						</ul>
+					</li>
+					<li className="ml-18d">
+						<ul>
+							<T.Title size={"xl"} as="h2">
+								Overlays
+							</T.Title>
+							<li className="ml-18d">
+								<Link href={"/components/overlays/popover"}>Popover</Link>
+							</li>
+							<li className="ml-18d">
+								<Link href={"/components/overlays/dialog"}>Dialog</Link>
+							</li>
+						</ul>
+					</li>
+
+					<li className="ml-18d">
+						<ul>
+							<T.Title size={"xl"} as="h2">
+								Typography
+							</T.Title>
+							<li className="ml-18d">
+								<Link href={"/components/typography"}>Typography</Link>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
 		</div>
 	);
 }
---
+++ apps/website/src/app/sandbox/page.tsx
@@ -0,0 +1,50 @@
+import { Toggle } from "@base-ui/react/toggle";
+import { ToggleGroup } from "@base-ui/react/toggle-group";
+import { Sandbox } from "@/components/sandbox/sandbox";
+import { T } from "@/components/ui/typography";
+
+export default function InputPage() {
+	return (
+		<Sandbox>
+			<div className="flex flex-col gap-4 -base-elevation-10 zero-elevation-0">
+				<T.Title>BUTTON</T.Title>
+				<div className="flex gap-16d">
+					<button
+						type="button"
+						className="trigger lifted-trigger sfc-ia sfc-color-accent-on-accent sfc-emphasis-high-on-high sfc-border w-16 rounded-12d before:rounded-16d"
+					>
+						o
+					</button>
+				</div>
+				<div className="flex gap-4">
+					<ToggleGroup
+						defaultValue={["left"]}
+						className="flex gap-px rounded-md border border-gray-200 bg-gray-50 p-0.5"
+					>
+						<Toggle
+							aria-label="Align left"
+							value="left"
+							className="flex size-8 items-center justify-center rounded-xs text-gray-600 select-none hover:bg-gray-100 focus-visible:bg-none focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 active:bg-gray-200 data-[pressed]:bg-gray-100 data-[pressed]:text-gray-900"
+						>
+							L
+						</Toggle>
+						<Toggle
+							aria-label="Align center"
+							value="center"
+							className="flex size-8 items-center justify-center rounded-xs text-gray-600 select-none hover:bg-gray-100 focus-visible:bg-none focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 active:bg-gray-200 data-[pressed]:bg-gray-100 data-[pressed]:text-gray-900"
+						>
+							C
+						</Toggle>
+						<Toggle
+							aria-label="Align right"
+							value="right"
+							className="flex size-8 items-center justify-center rounded-xs text-gray-600 select-none hover:bg-gray-100 focus-visible:bg-none focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 active:bg-gray-200 data-[pressed]:bg-gray-100 data-[pressed]:text-gray-900"
+						>
+							R
+						</Toggle>
+					</ToggleGroup>
+				</div>
+			</div>
+		</Sandbox>
+	);
+}
