f33c6879216261e999cea55d56eda6987aab96b4
playoffthecuff <jmobile@bk.ru>
2026-05-12 07:58:02 +0300

    refactor: material ds label

--- apps/website/src/components/ui/data-entry/fields/label.tsx
+++ apps/website/src/components/ui/data-entry/fields/label.tsx
@@ -1,18 +1,25 @@
+import type { FlattenIntersection } from "@/lib/types/helpers";
 import { cn } from "@/lib/utils/cn";
 import type { CompoundProps } from "../../types";
 import { Variants } from "../../variants";
 
-export type LabelProps = {
-	id?: string;
-	name?: string;
-	required?: boolean;
-};
+export namespace FieldLabel {
+	export type Variants = Variants.EmphasisSurface;
+	export type Props = FlattenIntersection<
+		Variants &
+			CompoundProps & {
+				id?: string;
+				name?: string;
+				required?: boolean;
+			}
+	>;
+}
 
 export const FieldLabel = ({
 	children,
 	className,
 	...props
-}: LabelProps & CompoundProps & Variants.EmphasisSurface) => (
+}: FieldLabel.Props) => (
 	<label
 		className={cn(
 			"leading-[1.25] sfc-text sfc-color-default-on-default",
