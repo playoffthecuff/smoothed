c5e3a9ecd1ab27dc4966875f6cb877ecd7dd8d9d
playoffthecuff <jmobile@bk.ru>
2026-05-12 07:53:38 +0300

    refactor: material ds text field

--- apps/website/src/components/ui/data-entry/fields/text/parts.tsx
+++ apps/website/src/components/ui/data-entry/fields/text/parts.tsx
@@ -1,17 +1,18 @@
 "use client";
 
 import { cva, type VariantProps } from "class-variance-authority";
-import type { ReactNode } from "react";
 import { createContext, useContext } from "react";
 import { SpinnerIcon } from "@/components/ui/icons/spinner";
 import { Popover } from "@/components/ui/overlays/popover";
 import { HintButton } from "@/components/ui/triggers/hint-button";
 import type { CompoundProps } from "@/components/ui/types";
-import { Variants } from "@/components/ui/variants";
-import type { AssignableProps, FlattenIntersection } from "@/lib/types/helpers";
+import { Variants as SharedVariants } from "@/components/ui/variants";
+import type { FlattenIntersection } from "@/lib/types/helpers";
 import { cn } from "@/lib/utils/cn";
-import { FieldLabel, type LabelProps } from "../label";
+import { FieldLabel } from "../label";
 import { FieldMessage } from "../message";
+
+export { Popover } from "../popover";
 
 const inputVariants = cva(
 	"justify-end focus-within:after:ring-6d transition-all lowered-text-input",
@@ -51,82 +52,72 @@ const inputVariants = cva(
 		},
 	},
 );
-type InputVariants = VariantProps<typeof inputVariants>;
-
-export type InputProps = FlattenIntersection<{
-	type?: "text" | "password" | "search" | "email" | "tel" | "url";
-}> &
-	Omit<React.ComponentProps<"input">, "type">;
-
-export type TextInputProps = FlattenIntersection<InputVariants & InputProps>;
-
-type CommonProps = AssignableProps<LabelProps, InputProps>;
-
-export type TextFieldProps = FlattenIntersection<
-	InputVariants &
-		Variants.EmphasisSurface &
-		Variants.IntentSurface &
-		Variants.Size &
-		Variants.InputWidth &
-		Variants.SurfaceCursor &
-		Variants.SurfaceShape &
-		CommonProps
+export type Variants = VariantProps<typeof inputVariants> &
+	SharedVariants.EmphasisSurface &
+	SharedVariants.IntentSurface &
+	SharedVariants.Size &
+	SharedVariants.InputWidth &
+	SharedVariants.SurfaceCursor &
+	SharedVariants.SurfaceShape;
+export type Props = FlattenIntersection<
+	Variants & {
+		type?: "text" | "password" | "search" | "email" | "tel" | "url";
+	} & Omit<
+			React.ComponentProps<"input">,
+			"type" | "className" | "children" | "width"
+		> &
+		CompoundProps
 >;
 
-const TextFieldContext = createContext<TextFieldProps | null>(null);
+const TextFieldContext = createContext<Variants | null>(null);
 
-function useTextFieldProps() {
+const useTextFieldProps = () => {
 	const ctx = useContext(TextFieldContext);
 	if (!ctx) throw new Error("Must be inside TextFieldContext.Provider");
 	return ctx;
-}
+};
 
-export function Root({
-	children,
-	className,
-	...props
-}: TextFieldProps & CompoundProps) {
-	return (
-		<TextFieldContext.Provider value={props}>
-			<div
-				className={cn(
-					Variants.fontSizeVariants(props),
-					Variants.semiBoldFontVariants(props),
+export const Root = ({ children, className, ...props }: Props) => (
+	<TextFieldContext.Provider value={props}>
+		<div
+			className={cn(
+				SharedVariants.fontSizeVariants(props),
+				SharedVariants.semiBoldFontVariants(props),
 
-					"flex-col",
-					props.width === "fill" ? "flex" : "inline-flex w-min",
-					"justify-center items-center",
-					className,
-				)}
-			>
-				{children}
-			</div>
-		</TextFieldContext.Provider>
-	);
-}
-export const Control = ({ className, children }: CompoundProps) => {
-	const props = useTextFieldProps();
+				"flex-col",
+				props.width === "fill" ? "flex" : "inline-flex w-min",
+				"justify-center items-center",
+				className,
+			)}
+		>
+			{children}
+		</div>
+	</TextFieldContext.Provider>
+);
+
+export const Control = ({ className, children, ...props }: Props) => {
+	const mergedProps = { ...useTextFieldProps(), ...props };
 	return (
 		<div
 			className={cn(
 				"flex relative justify-end items-center",
-				Variants.surfaceCursorVariants(props),
-				props.loading && !props.disabled && "shimmer-bg",
+				SharedVariants.surfaceCursorVariants(mergedProps),
+				mergedProps.loading && !mergedProps.disabled && "shimmer-bg",
 			)}
 		>
 			<div
 				className={cn(
-					inputVariants(props),
-					Variants.emphasisSurfaceVariants(props),
-					Variants.interactiveIntentSurfaceVariants(props),
-					Variants.inputWidthVariants(props),
-					Variants.surfaceShapeVariants(props),
+					inputVariants(mergedProps),
+					SharedVariants.emphasisSurfaceVariants(mergedProps),
+					SharedVariants.interactiveIntentSurfaceVariants(mergedProps),
+					SharedVariants.inputWidthVariants(mergedProps),
+					SharedVariants.surfaceShapeVariants(mergedProps),
 					className,
 				)}
 			>
 				{children}
 			</div>
-			{props.loading && props.disabled && (
+			{mergedProps.loading && mergedProps.disabled && (
 				<div className="h-full absolute flex items-center justify-center aspect-square pointer-events-none">
 					<SpinnerIcon className="animate-spin" />
 				</div>
@@ -135,52 +126,59 @@ export const Control = ({ className, children }: CompoundProps) => {
 	);
 };
 
-export const Input = ({
-	defaultValue,
-	type,
-	placeholder,
-	className,
-}: InputProps) => {
-	const props = useTextFieldProps();
+export const Input = ({ className, children, ...props }: Props) => {
+	const {
+		emphasis,
+		intent,
+		flat,
+		loading,
+		outlined,
+		solid,
+		disabled,
+		ringed,
+		size,
+		shape,
+		...ctxProps
+	} = useTextFieldProps();
 	return (
 		<input
 			className={cn(
 				"text-input-box sfc-intent-selection",
-				props.loading && "not-focus:cursor-wait",
+				loading && "not-focus:cursor-wait",
 				className,
 			)}
-			type={type}
-			id={props.id}
-			defaultValue={defaultValue}
-			disabled={props.disabled ?? undefined}
-			placeholder={placeholder}
+			disabled={disabled || props.disabled}
+			{...ctxProps}
+			{...props}
+			width={undefined}
 		/>
 	);
 };
 
-export const InputContent = ({ children, className }: CompoundProps) => {
-	return (
-		<div
-			className={cn(
-				"relative h-full flex items-center justify-center",
-				className,
-			)}
-		>
-			{children}
-		</div>
-	);
-};
+export const InputContent = ({ children, className }: CompoundProps) => (
+	<div
+		className={cn(
+			"relative h-full flex items-center justify-center",
+			className,
+		)}
+	>
+		{children}
+	</div>
+);
 
-export const Hint = ({ children, className }: CompoundProps) => {
-	const props = useTextFieldProps();
+export const Hint = ({
+	children,
+	className,
+	...props
+}: CompoundProps & Variants) => {
+	const ctxProps = useTextFieldProps();
 	return (
 		<InputContent className={cn("aspect-square", className)}>
 			<HintButton
 				className="rounded-s-none"
+				{...ctxProps}
 				{...props}
 				loading={undefined}
-				openOnHover
-				delay={150}
 			>
 				{children}
 			</HintButton>
@@ -188,57 +186,57 @@ export const Hint = ({ children, className }: CompoundProps) => {
 	);
 };
 
-export const Label = ({ className, children }: CompoundProps) => {
-	const props = useTextFieldProps();
+export const Label = ({ className, children, ...props }: FieldLabel.Props) => {
+	const mergedProps = { ...useTextFieldProps(), ...props };
 	return (
 		<FieldLabel
 			className={cn(
 				"w-full",
-				props.shape === "circular" && "first-letter:ms-[.525em]",
-				props.required && "before:content-['✺_'] before:sfc-text-danger-450d",
+				mergedProps.shape === "circular" && "first-letter:ms-[.525em]",
+				mergedProps.required &&
+					"before:content-['✺_'] before:sfc-text-danger-450d",
 				className,
 			)}
-			required={props.required}
-			emphasis={props.emphasis}
+			{...mergedProps}
 		>
 			{children}
 		</FieldLabel>
 	);
 };
 
-export const Message = ({ children, className }: CompoundProps) => {
-	const props = useTextFieldProps();
+export const Message = ({
+	children,
+	className,
+	...props
+}: FieldMessage.Props) => {
+	const mergedProps = { ...useTextFieldProps(), ...props };
 	return (
 		<FieldMessage
 			className={cn(
 				"h-[3.5em] -mt-[2em] pt-[2em] w-full transition-all duration-500",
 				!children && "bg-transparent",
-				(props.shape === "circular" || props.solid || props.outlined) &&
+				(mergedProps.shape === "circular" ||
+					mergedProps.solid ||
+					mergedProps.outlined) &&
 					"ps-[.525em]",
 				className,
 			)}
-			{...props}
+			{...mergedProps}
 		>
 			{children}
 		</FieldMessage>
 	);
 };
 
-interface PopoverProps extends CompoundProps {
-	open?: boolean;
-}
-
-const PopoverRoot = ({ children, open, className }: PopoverProps) => {
+export const PopoverMessage = ({
+	children,
+	className,
+	...props
+}: Popover.Portal.Props) => {
+	const mergedProps = { ...useTextFieldProps(), ...props };
 	return (
-		<Popover.Root open={open}>
-			<Popover.Trigger className={className}>{children}</Popover.Trigger>
-		</Popover.Root>
+		<Popover.Portal {...mergedProps} className={className}>
+			{children}
+		</Popover.Portal>
 	);
-};
-
-export { PopoverRoot as Popover };
-
-export const PopoverMessage = ({ children }: CompoundProps) => {
-	const props = useTextFieldProps();
-	return <Popover.Portal {...props}>{children}</Popover.Portal>;
 };
