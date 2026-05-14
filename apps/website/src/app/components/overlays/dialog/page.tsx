446997ae5daca5cfa26520d5a8019bfd485ba7fd
playoffthecuff <jmobile@bk.ru>
2026-05-14 13:58:25 +0300

    refactor: dialog page

--- apps/website/src/app/components/overlays/dialog/page.tsx
+++ apps/website/src/app/components/overlays/dialog/page.tsx
@@ -3,9 +3,16 @@ import { HighlightedCode } from "@/components/ui/highlighted-code";
 import { Dialog } from "@/components/ui/overlays/dialog";
 import { Button } from "@/components/ui/triggers/button";
 import { T } from "@/components/ui/typography";
-import { emphases, intentColors } from "@/design-systems/material/options";
 
 export default function DialogPage() {
+	const variantsCfg = {
+		shape: ["square", "rounded", "circular"],
+		emphasis: ["low", "medium", "high"],
+		intent: ["neutral", "accent", "success", "warning", "danger", "info"],
+		size: ["xs", "s", "m", "l", "xl"],
+		id: [""],
+		width: ["narrow", "normal", "wide", "fill"],
+	} as const;
 	return (
 		<Sandbox>
 			<div>
@@ -24,42 +31,8 @@ export default function DialogPage() {
 			</div>
 			<hr className="mt-16d mb-12d" />
 			<div className="grid grid-cols-[repeat(3,max-content)] gap-16d">
-				{/* <div className="flex justify-between gap-16d items-center">
-					<div className="grow capitalize">No Intent</div>
-					<Dialog.Root>
-						<Dialog.Trigger>Low</Dialog.Trigger>
-						<Dialog.Portal className={"p-[var(--padding)]"} emphasis="low">
-							<Dialog.Header>Header</Dialog.Header>
-							<Dialog.Title>Title</Dialog.Title>
-							<Dialog.Description>Description</Dialog.Description>
-							<Dialog.Close>Close</Dialog.Close>
-						</Dialog.Portal>
-					</Dialog.Root>
-				</div> */}
-				{/* <div className="flex justify-between gap-16d items-center">
-						<Dialog.Root>
-							<Dialog.Trigger>Medium</Dialog.Trigger>
-							<Dialog.Portal className={"p-[var(--padding)]"} emphasis="medium">
-								<Dialog.Header>Header</Dialog.Header>
-								<Dialog.Title>Title</Dialog.Title>
-								<Dialog.Description>Description</Dialog.Description>
-								<Dialog.Close>Close</Dialog.Close>
-							</Dialog.Portal>
-						</Dialog.Root>
-					</div> */}
-				{/* <div className="flex justify-between gap-16d items-center">
-					<Dialog.Root>
-						<Dialog.Trigger>High</Dialog.Trigger>
-						<Dialog.Portal className={"p-[var(--padding)]"} emphasis="high">
-							<Dialog.Header>Header</Dialog.Header>
-							<Dialog.Title>Title</Dialog.Title>
-							<Dialog.Description>Description</Dialog.Description>
-							<Dialog.Close>Close</Dialog.Close>
-						</Dialog.Portal>
-					</Dialog.Root>
-				</div> */}
-				{intentColors.map((intent) =>
-					emphases.map((emphasis, i) => (
+				{variantsCfg.intent.map((intent) =>
+					variantsCfg.emphasis.map((emphasis, i) => (
 						<div
 							key={`${intent}${emphasis}`}
 							className="flex justify-between gap-16d items-center"
@@ -78,11 +51,7 @@ export default function DialogPage() {
 										{emphasis}
 									</Button>
 								</Dialog.Trigger>
-								<Dialog.Portal
-									className={"p-[var(--padding)]"}
-									intent={intent}
-									emphasis={emphasis}
-								>
+								<Dialog.Portal intent={intent} emphasis={emphasis}>
 									<Dialog.Header>Header</Dialog.Header>
 									<Dialog.Title>Title</Dialog.Title>
 									<Dialog.Description>Description</Dialog.Description>
@@ -94,8 +63,8 @@ export default function DialogPage() {
 				)}
 			</div>
 			<div className="grid grid-cols-[repeat(3,max-content)] gap-16d">
-				{intentColors.map((intent) =>
-					emphases.map((emphasis, i) => (
+				{variantsCfg.intent.map((intent) =>
+					variantsCfg.emphasis.map((emphasis, i) => (
 						<div
 							key={`${intent}${emphasis}-2`}
 							className="flex justify-between gap-16d items-center"
@@ -114,7 +83,6 @@ export default function DialogPage() {
 									</Button>
 								</Dialog.Trigger>
 								<Dialog.Portal
-									className={"p-[var(--padding)]"}
 									intent={intent}
 									emphasis={emphasis}
 									solid
